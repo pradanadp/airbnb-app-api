02c20424d14ec3d16851fc279e7fe44fd0ff99e5
pradanadp <pradana.dimasdp@gmail.com>
2023-06-14 19:10:11 +0700

    Update files

--- features/image/image_interfaces.go
+++ features/image/image_interfaces.go
@@ -6,12 +6,12 @@ import (
 
 type ImageRepository interface {
 	Insert(image models.ImageEntity) (uint, error)
-	Select(imageID uint) (models.ImageEntity, error)
+	SelectAll(homestayID uint) ([]models.ImageEntity, error)
 	Delete(imageID uint) error
 }
 
 type ImageService interface {
 	CreateImage(image models.ImageEntity) (uint, error)
-	GetImage(imageID uint) (models.ImageEntity, error)
+	GetImage(homestayID uint) ([]models.ImageEntity, error)
 	DeleteImage(imageID uint) error
 }
